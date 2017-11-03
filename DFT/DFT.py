1ff2faa0401cc5a3f65927cb2362769af2647626
gthomp <cbk1011.gt@gmail.com>
2017-11-02 23:30:03 -0500

    updated DFT files

--- DFT/DFT.py
+++ DFT/DFT.py
@@ -1,19 +1,33 @@
 # For this part of the assignment, please implement your own code for all computations,
 # Do not use inbuilt functions like fft from either numpy, opencv or other libraries
 
+import copy
+import numpy as np
+
 
 class DFT:
-
     def forward_transform(self, matrix):
         """Computes the forward Fourier transform of the input matrix
         takes as input:
         matrix: a 2d matrix
         returns a complex matrix representing fourier transform"""
 
+        img_ = copy.deepcopy(matrix)
+        rows = img_.shape[0]
+        cols = img_.shape[1]
 
+        fft_ = np.zeros(matrix.shape, dtype=complex)
+        for k in range(rows):
+            for l in range(cols):
+                outer_sum_ = 0
+                for n in range(cols):
+                    inner_sum_ = 0
+                    for m in range(rows):
+                        inner_sum_ += img_[m, n] * np.exp(-2.0j * np.pi * m * k / rows)
+                    outer_sum_ += inner_sum_ * np.exp(-2.0j * np.pi * n * l / cols)
+                fft_[k, l] = outer_sum_
 
-
-        return matrix
+        return fft_
 
     def inverse_transform(self, matrix):
         """Computes the inverse Fourier transform of the input matrix
@@ -21,10 +35,22 @@ class DFT:
         takes as input:
         returns a complex matrix representing the inverse fourier transform"""
 
+        fft_ = copy.deepcopy(matrix)
+        rows = fft_.shape[0]
+        cols = fft_.shape[1]
 
+        img_ = np.zeros(matrix.shape, dtype=complex)
+        for m in range(rows):
+            for n in range(cols):
+                outer_sum_ = 0
+                for l in range(cols):
+                    inner_sum_ = 0
+                    for k in range(rows):
+                        inner_sum_ += fft_[k, l] * np.exp(2.0j * np.pi * k * m / rows)
+                    outer_sum_ += inner_sum_ * np.exp(2.0j * np.pi * l * n / cols)
+                img_[m, n] = outer_sum_
 
-        return matrix
-
+        return img_
 
     def discrete_cosine_tranform(self, matrix):
         """Computes the discrete cosine transform of the input matrix
@@ -32,10 +58,7 @@ class DFT:
         matrix: a 2d matrix
         returns a matrix representing discrete cosine transform"""
 
-
-
         return matrix
-
 
     def magnitude(self, matrix):
         """Computes the magnitude of the DFT
@@ -43,4 +66,4 @@ class DFT:
         matrix: a 2d matrix
         returns a matrix representing magnitude of the dft"""
 
-        return matrix
+        return np.log(1 + np.abs(matrix))
--- DFT/Filtering.py
+++ DFT/Filtering.py
@@ -1,6 +1,8 @@
 # For this part of the assignment, You can use inbuilt functions to compute the fourier transform
 # You are welcome to use fft that are available in numpy and opencv
 
+import numpy as np
+
 
 class Filtering:
     image = None
@@ -8,7 +10,7 @@ class Filtering:
     cutoff = None
     order = None
 
-    def __init__(self, image, filter_name, cutoff, order = 0):
+    def __init__(self, image, filter_name, cutoff, order=0):
         """initializes the variables frequency filtering on an input image
         takes as input:
         image: the input image
@@ -33,29 +35,30 @@ class Filtering:
         self.cutoff = cutoff
         self.order = order
 
-
     def get_ideal_low_pass_filter(self, shape, cutoff):
         """Computes a Ideal low pass mask
         takes as input:
         shape: the shape of the mask to be generated
         cutoff: the cutoff frequency of the ideal filter
-        returns a ideal low pass mask"""
+        returns an ideal low pass mask"""
+        rows, cols = shape
+        x = np.linspace(-0.5, 0.5, cols) * cols
+        y = np.linspace(-0.5, 0.5, rows) * rows
+        radius = np.sqrt((x ** 2)[np.newaxis] + (y ** 2)[:, np.newaxis])
+        mask = np.zeros(shape)
+        mask[radius < cutoff] = 1
+        print(radius.astype(int))
 
-
-        return 0
-
+        return mask
 
     def get_ideal_high_pass_filter(self, shape, cutoff):
         """Computes a Ideal high pass mask
         takes as input:
         shape: the shape of the mask to be generated
         cutoff: the cutoff frequency of the ideal filter
-        returns a ideal high pass mask"""
-
-        #Hint: May be one can use the low pass filter function to get a high pass mask
-
-        
-        return 0
+        returns a ideal high pass mask
+        Hint: May be one can use the low pass filter function to get a high pass mask"""
+        return 1.0 - self.get_ideal_low_pass_filter(shape, cutoff)
 
     def get_butterworth_low_pass_filter(self, shape, cutoff, order):
         """Computes a butterworth low pass mask
@@ -64,9 +67,12 @@ class Filtering:
         cutoff: the cutoff frequency of the butterworth filter
         order: the order of the butterworth filter
         returns a butterworth low pass mask"""
-
-        
-        return 0
+        rows, cols = shape
+        x = np.linspace(-0.5, 0.5, cols) * cols / 1
+        y = np.linspace(-0.5, 0.5, rows) * rows / 1
+        radius = np.sqrt((x ** 2)[np.newaxis] + (y ** 2)[:, np.newaxis])
+        mask = 1 / (1.0 + (radius / cutoff) ** (2 * order))
+        return mask
 
     def get_butterworth_high_pass_filter(self, shape, cutoff, order):
         """Computes a butterworth high pass mask
@@ -74,12 +80,9 @@ class Filtering:
         shape: the shape of the mask to be generated
         cutoff: the cutoff frequency of the butterworth filter
         order: the order of the butterworth filter
-        returns a butterworth high pass mask"""
-
-        #Hint: May be one can use the low pass filter function to get a high pass mask
-
-        
-        return 0
+        returns a butterworth high pass mask
+        Hint: May be one can use the low pass filter function to get a high pass mask"""
+        return 1. - self.get_butterworth_low_pass_filter(shape, cutoff, order)
 
     def get_gaussian_low_pass_filter(self, shape, cutoff):
         """Computes a gaussian low pass mask
@@ -87,21 +90,26 @@ class Filtering:
         shape: the shape of the mask to be generated
         cutoff: the cutoff frequency of the gaussian filter (sigma)
         returns a gaussian low pass mask"""
-
-        
-        return 0
+        rows, cols = shape
+        x = np.linspace(-0.5, 0.5, cols) * cols
+        y = np.linspace(-0.5, 0.5, rows) * rows
+        radius = (x ** 2)[np.newaxis] + (y ** 2)[:, np.newaxis]
+        mask = 1 / (2 * np.pi * cutoff ** 2) * np.exp(-1 * radius / (2 * cutoff ** 2))
+        return mask
 
     def get_gaussian_high_pass_filter(self, shape, cutoff):
         """Computes a gaussian high pass mask
         takes as input:
         shape: the shape of the mask to be generated
         cutoff: the cutoff frequency of the gaussian filter (sigma)
-        returns a gaussian high pass mask"""
-
-        #Hint: May be one can use the low pass filter function to get a high pass mask
-
-        
-        return 0
+        returns a gaussian high pass mask
+        Hint: May be one can use the low pass filter function to get a high pass mask"""
+        rows, cols = shape
+        x = np.linspace(-0.5, 0.5, cols) * cols
+        y = np.linspace(-0.5, 0.5, rows) * rows
+        radius = (x ** 2)[np.newaxis] + (y ** 2)[:, np.newaxis]
+        mask = 1 / (2 * np.pi * cutoff ** 2) * np.exp(-1 * radius / (2 * cutoff ** 2))
+        return 1 - mask
 
     def post_process_image(self, image):
         """Post process the image to create a full contrast stretch of the image
@@ -110,12 +118,14 @@ class Filtering:
         return an image with full contrast stretch
         -----------------------------------------------------
         1. Full contrast stretch (fsimage)
-        2. take negative (255 - fsimage)
-        """
-
-
-        return image
-
+        2. take negative (255 - fsimage)"""
+        intensity_lower = np.amin(image)  # a
+        intensity_upper = np.amax(image)  # b
+        ideal_lower = 0  # c
+        ideal_upper = 255  # d
+        stretched = (image - ideal_lower) \
+                    * ((intensity_upper - intensity_lower) / (ideal_upper - ideal_lower)) + intensity_lower
+        return stretched
 
     def filtering(self):
         """Performs frequency filtering on an input image
@@ -126,18 +136,29 @@ class Filtering:
         Steps:
         1. Compute the fft of the image
         2. shift the fft to center the low frequencies
-        3. get the mask (write your code in functions provided above) the functions can be called by self.filter(shape, cutoff, order)
+        3. get the mask (write your code in functions provided above) the functions can be called by self.filter(shape,
+            cutoff, order)
         4. filter the image frequency based on the mask (Convolution theorem)
         5. compute the inverse shift
         6. compute the inverse fourier transform
         7. compute the magnitude
-        8. You will need to do a full contrast stretch on the magnitude and depending on the algorithm you may also need to
-        take negative of the image to be able to view it (use post_process_image to write this code)
+        8. You will need to do a full contrast stretch on the magnitude and depending on the algorithm you may also
+            need to take negative of the image to be able to view it (use post_process_image to write this code)
         Note: You do not have to do zero padding as discussed in class, the inbuilt functions takes care of that
-        filtered image, magnitude of DFT, magnitude of filtered DFT: Make sure all images being returned have grey scale full contrast stretch and dtype=uint8 
+        filtered image, magnitude of DFT, magnitude of filtered DFT: Make sure all images being returned have grey
+            scale full contrast stretch and dtype=uint8
         """
+        img_ = self.image
+        forward_ = np.fft.fftshift(np.fft.fft2(img_))
 
+        if self.order == 0:
+            mask_ = self.filter(forward_.shape, self.cutoff)
+        else:
+            mask_ = self.filter(forward_.shape, self.cutoff, self.order)
+        filtered_ = forward_ * mask_
 
-
-
-        return [self.image, self.image, self.image]
+        inverse_ = np.fft.ifft2(np.fft.ifftshift(filtered_))
+        postproc_ = self.post_process_image(inverse_)
+        mag_forward_ = np.log(1 + np.abs(forward_))
+        mag_filtered_ = np.log(1 + np.abs(filtered_))
+        return [postproc_, mag_forward_, mag_filtered_]
